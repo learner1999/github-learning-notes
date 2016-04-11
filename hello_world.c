946fc80c89bb4231d7005af5d08147bc5c7c10f8
learner1999 <roadoflearning@live.com>
2016-04-11 19:46:26 +0800

    Add hello world script by c

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hello_world.c
+	> Author: learner1999 
+	> Mail: roadoflearning@live.com
+	> Created Time: 2016年04月11日 星期一 19时23分16秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(void)
+{
+    printf("hello world");
+
+    return 0;
+}
